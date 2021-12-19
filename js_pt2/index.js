37fad8694a43f7b856a9a0ea419a4af53d295760
BrunoSPimenta <bruno.s.pimenta@hotmail.com>
2021-12-19 15:19:25 -0300

    avançando na aula

--- js_pt2/index.js
+++ js_pt2/index.js
@@ -1,48 +1,27 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    //#saldo = propriedade privada https://github.com/tc39/proposal-class-fields#private-fields
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;            
-        }
-        this._saldo += valor;
-    }
-}
+import{ Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf= 11122233309;
+
+const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(50);
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
+contaCorrenteRicardo.transferir(200, conta2)
+
+console.log(conta2);
 
 console.log(contaCorrenteRicardo);
-
