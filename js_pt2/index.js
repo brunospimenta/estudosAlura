43350e02766814ec98fd2186956f50aa9976dbaf
BrunoSPimenta <bruno.s.pimenta@hotmail.com>
2021-12-19 14:53:05 -0300

    curso js orientado a objeto

---
+++ js_pt2/index.js
@@ -0,0 +1,48 @@
+class Cliente{
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
+
+    //#saldo = propriedade privada https://github.com/tc39/proposal-class-fields#private-fields
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;            
+        }
+        this._saldo += valor;
+    }
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf= 11122233309;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+
+console.log(contaCorrenteRicardo);
+
