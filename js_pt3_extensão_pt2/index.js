8398929f5f83bd3f645f31e3be0112d27483fdf1
BrunoSPimenta <bruno.s.pimenta@hotmail.com>
2021-12-21 01:48:00 -0300

    Final do Cursos Herança (JS orientado a objeto)

--- js_pt3_extensão_pt2/index.js
+++ js_pt3_extensão_pt2/index.js
@@ -1,14 +1,25 @@
 import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js"
+import { Diretor } from "./Funcionarios/Diretor.js"
+import { SistemaAutenticacao } from "./Funcionarios/SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const gerente = new Gerente("Ricardo", 5000, 12378945601)
+gerente.cadastrarSenha("123")
 
-console.log(contaSalario);
+const cliente = new Cliente("Lais", 78945612379, "456");
+
+
+const gerenteLogado = SistemaAutenticacao.login(gerente, "123")
+const diretorLogado = SistemaAutenticacao.login(diretor, "123456")
+
+console.log(gerenteLogado);
+console.log(diretorLogado);
+
+const clienteLogado = SistemaAutenticacao.login(cliente, "456")
+
+console.log(clienteLogado)
+
+
