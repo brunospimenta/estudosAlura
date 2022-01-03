b9d489cc5a355d1b766d24607ae963a7b9b8f7d1
BrunoSPimenta <bruno.s.pimenta@hotmail.com>
2022-01-02 23:59:41 -0300

    pt2 do curso typescript2

--- TypeScript/app/views/negociacoes-view.ts
+++ TypeScript/app/views/negociacoes-view.ts
@@ -1,12 +1,8 @@
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
+export class NegociacoesView extends View<Negociacoes> {
 
-    private elemento: HTMLElement;
-
-    constructor(seletor: string){        
-        this.elemento = document.querySelector(seletor);
-    }
     template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
@@ -32,9 +28,4 @@ export class NegociacoesView {
         `;
     }
 
-    update(model: Negociacoes): void{
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML =this.template(model);
-    }
 }
