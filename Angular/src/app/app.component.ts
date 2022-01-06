33e14eab47cfe00c70d6b3c412285311fea64926
BrunoSPimenta <bruno.s.pimenta@hotmail.com>
2022-01-05 21:59:22 -0300

    Começo do curso de Angular

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Alurapic';
+  description= 'teste';
+  url="https://www.ifrr.edu.br/midia/teste/image";
+}
